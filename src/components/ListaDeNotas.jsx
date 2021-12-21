e83c779eb53c2b8b96e68e136fff3c476b8aabcf
Alexandre <prudenteale1@gmail.com>
2021-12-21 15:36:56 -0300

    primeiro commit

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from "react"
+
+export default class ListaDeNotas extends Component{
+    render(){
+        return(
+        <ul>
+            <li>
+              <section>
+              <header>
+            <h2>Texto aqui</h2>
+              </header>
+              <p>olá</p>
+              </section>
+            </li>
+          </ul>
+
+        )
+    }
+
+}
+ 
